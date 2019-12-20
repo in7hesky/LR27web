138e72e09d48096f9b6b51d4396d709694499d0e
in7hesky <freonzet@gmail.com>
2019-12-20 05:01:02 +0300

    Second

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 $(function() {
     $('.start-btn').click(function(event) {
-        let start = aim.targetStart.bind(aim)
-        start()
         event.target.style.visibility = "hidden"
-        $('.main-header').text("Difficulty 1")
+        $('.main-header').html("Difficulty: <span class='difficulty-level'>1</span>")
+        startEvents()
     })
+
+    function startEvents() {
+        let start = aim.targetStart.bind(aim)
+        setTimeout(() => start(), 4700)
+
+        let counter = 3
+        let countdown = document.querySelector(".countdown")
+        countdown.style.opacity = '1'
+        setTimeout(() => {
+            countdown.style.opacity = '0'
+            countdown.style.zIndex = '-1'
+        }, 4700)
+        let countdownIteration = setInterval(() => {
+            if (counter == 0) {
+                $('.countdown').text("GO!")
+                clearInterval(countdownIteration)
+                return
+            }
+            $(".countdown").text(`${counter}`)
+            counter--
+        }, 1000)
+        
+    }
 
 
     let aim = {
@@ -12,21 +34,48 @@ $(function() {
         pointsObj: $('.points'),
         score: 0,
         speed: 15,
+        clicks: 0,
 
         targetStart() {
+            timer.start()
+            $(".clicks").show()
             this.targetObj.addClass("target-motion target--ingame")
             this.targetObj.mousedown(() => {
                 this.incrementLevel()
             })
+            $(".play-field").click(() => {
+                this.incrementClicks()
+            })
+        },
+
+        incrementClicks() {
+            if (this.clicks == 0) {
+                this.offsetOfBar = document.querySelector(".clicks__amount").offsetWidth
+                this.widthToRemove = document.querySelector(".clicks__amount").offsetWidth / 20
+            }
+            this.clicks++
+            document.querySelector(".clicks__amount").style.width = this.offsetOfBar - this.widthToRemove * this.clicks + 'px'
+
+            if (this.clicks == 10) {
+                document.querySelector(".clicks__amount").style.backgroundColor = '#ff7b25'
+            }    
+            if (this.clicks == 20) {
+                document.querySelector(".clicks").style.color = '#ff7b25'
+                this.fail()
+            }
+            
+            
         },
 
         incrementLevel() {
             this.score++
             this.pointsObj.text(`${this.score}`)
             if (this.score == 5) {
-                this.changeDifficulty()
+                this.raiseDifficulty(2)
+            } else if (this.score == 10) {
+                this.raiseDifficulty(3)
             } else if (this.score == 15) {
-                this.finish()
+                this.win()
             } else {
                 this.addSpeed()
             }
@@ -38,18 +87,95 @@ $(function() {
             document.querySelector(".target-motion").style.animationDuration = `${this.speed}s`
         },
 
-        changeDifficulty() {
-            document.querySelector(".target-motion").style.animationName = "target-level-2"
-            $(".main-header").text("Difficulty 2")
-            this.targetObj.addClass("target--small")
+        raiseDifficulty(level) {
+            if (level == 2) {
+                document.querySelector(".target-motion").style.animationName = "target-level-2"
+                this.targetObj.addClass("target--small")
+            } else if (level == 3) {
+                this.showTargetInterval = setInterval(() => {
+                    let targetNode = document.querySelector(".target")
+                    if (targetNode.style.opacity == "0") {
+                        targetNode.style.opacity = '1'
+                    } else {
+                        targetNode.style.opacity = "0"
+                    }
+                }, 500)
+            }
+            
+            $(".difficulty-level").text(`${level}`)
+            
         },
 
         finish() {
-            $(".play-field").html("<div style='font-size: 4rem; text-align: center;'>You rock!</div>")
+            timer.stop()
+            clearInterval(this.showTargetInterval)
+            $(".play-field").unbind()   
+        },
+
+        win() {
+            this.finish()
+
+            let playtime = timer.getPlaytime()
+            $(".play-field").html(`<div class='result'>You rock!<br>Playtime:<br> <span class='points'>${playtime}</span></div>`)
             $(".main-header").text("Congratulations!")
             $(".play-field").css({
                             background: 'greenyellow'})
-        }
+        },
 
+        fail() {
+            this.finish()
+
+            $(".play-field").html("<div class='result'>Failed</div>")
+            $(".play-field").css({
+                background: 'tomato'
+            })
+
+
+        }
     }
+
+    let timer = {
+        seconds: 0,
+
+        start() {
+            this.interval = setInterval(() => this.seconds++, 1000)
+        },
+
+        stop() {
+            clearInterval(this.interval)
+        },
+
+        getPlaytime() {
+            let timeArr = [0]
+            let seconds = this.seconds
+
+            if (seconds > 59) {
+                let minutes = Math.floor(this.seconds / 60)
+                seconds %= 60
+                timeArr.push(minutes)
+                if (minutes > 59) {
+                    let hours = Math.floor(minutes / 60)
+                    minutes %= 60
+                    timeArr.push(hours)
+                }
+            }
+
+            timeArr[0] = seconds
+
+            return this._getTimeString(timeArr)
+        },
+
+        _getTimeString(arr) {
+            let smhStringsArray = []
+
+            let partOfTimeStr;
+            for (let i of arr) {
+                partOfTimeStr = i < 10 ? `0${i}` : i.toString()
+                smhStringsArray.unshift(partOfTimeStr)
+            }
+
+            return arr.length == 1 ? smhStringsArray[0] + 'sec' : smhStringsArray.join(":")
+        }
+    }
+
 })
